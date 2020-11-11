bdaef788379266cbd6a76176dee9482eb541787e
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-11 12:40:23 +0100

    various upadates

--- devhistory.R
+++ devhistory.R
@@ -8,12 +8,18 @@ rrtools::use_compendium("/Users/rodenico/Documents/Pro/Articles/2020_EvolExp/Ana
 ## Create a R directory and a file for functions
 usethis::use_r("loadfitnessdata")
 usethis::use_r("simfitnessdiff")
+usethis::use_r("estim_overdisp")
+
+
 
 dir.create("data")
+dir.create("reports")
 
 ## Update DESCRIPTION file
 usethis::use_package("here")
 usethis::use_package("readr")
+usethis::use_package("MASS")
+usethis::use_package("fitdistrplus")
 
 ## Update NAMESPACE file
 devtools::document()
@@ -22,10 +28,4 @@ devtools::document()
 devtools::load_all()
 
 
-data_G0 <- loadfitnessdata(dataset = "Selection_Phenotypage_G0_G7_G8.csv", generation = "G1")
-data_G7 <- loadfitnessdata(dataset = "Selection_Phenotypage_G0_G7_G8.csv", generation = "G7")
-data_G29 <- loadfitnessdata(dataset = "PERFORMANCE_Comptage_adultes_G13G14G15G16G17G18G19G20G21G22G23G24G25G26G27G28G29.csv", generation = "29")
 
-head(data_G0)
-head(data_G7)
-head(data_G29)
