d522d7099c93f8488a5c11d2c2eaa5d20bbe531e
Nicolas O. Rode <nicolas.rode@inrae.fr>
2020-11-12 15:13:38 +0100

    :wolf: change function

--- devhistory.R
+++ devhistory.R
@@ -5,8 +5,12 @@ rrtools::use_compendium("/Users/rodenico/Documents/Pro/Articles/2020_EvolExp/Ana
 
 ## Add to .gitignore
 usethis::use_git_ignore(".DS_Store")
+usethis::use_build_ignore(".DS_Store")
+usethis::use_git(message = ":see_no_evil: Ban .DS_Store files")
 
 ## Modify DESCRIPTION file
+usethis::edit_file("DESCRIPTION")
+usethis::use_git(message = ":bulb: Update documentation")
 
 ## Create a R directory and a file for functions
 usethis::use_r("loadfitnessdata")
