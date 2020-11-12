abaf2a18af6a7f46d7e5c23c704ba28ddf8fb479
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-12 08:23:45 -0700

    :dog: order output

--- R/estim_overdisp.R
+++ R/estim_overdisp.R
@@ -6,7 +6,15 @@
 #' @param data Dataset considered
 #' @param generation Generation considered
 #'
-#' @return
+#' @return "number_tubes_counted",
+#'  "fitted_mean_nb_adults",
+#'  "fitted_theta",
+#'   "observed_var_nb_adults",
+#'   "fitted_var_nb_adults",
+#' "obsoverdisp=observed_var_nb_adults/observed_mean_adults",
+#' "fittedoverdisp==fitted_var_nb_adults/fitted_mean_nb_adults",
+#' "standardized_mean",
+#' "standardized_mean_small_sample"
 #' @export
 #'
 #' @examples
@@ -18,6 +26,14 @@ estim_overdisp <- function(colfactorlevel, colfactor, data, generation){
   ## Fit model
   m <- MASS::glm.nb(Nb_adults~1, data=data)
   ## Get parameters
-  param <- c(length(data$Nb_adults), as.numeric(exp(coef(m))), m$theta, var(data$Nb_adults), as.numeric(exp(coef(m))+(exp(coef(m))^2)/m$theta), var(data$Nb_adults)/mean(data$Nb_adults), 1+as.numeric(exp(coef(m)))/m$theta, ((4*length(data$Nb_adults)^(3/2))/(1+4*length(data$Nb_adults)))* (mean(data$Nb_adults)/sd(data$Nb_adults)), sqrt(length(data$Nb_adults))* (mean(data$Nb_adults)/sd(data$Nb_adults)))
+  param <- c(length(data$Nb_adults),
+             as.numeric(exp(coef(m))),
+             m$theta,
+             var(data$Nb_adults),
+             as.numeric(exp(coef(m))+(exp(coef(m))^2)/m$theta),
+             var(data$Nb_adults)/mean(data$Nb_adults),
+             1+as.numeric(exp(coef(m)))/m$theta,
+             sqrt(length(data$Nb_adults))*(mean(data$Nb_adults)/sd(data$Nb_adults)),
+             ((4*length(data$Nb_adults)^(3/2))/(1+4*length(data$Nb_adults)))* (mean(data$Nb_adults)/sd(data$Nb_adults)))
   as.vector(c(colfactorlevel, generation, round(param, 2)))
 }
