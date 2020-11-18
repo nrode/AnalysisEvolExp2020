04ec366c640a8b3716e7dd7e476144a1348511f5
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-18 10:15:46 -0700

    :fairy: add new theme

--- R/theme_LO_sober.R
+++ R/theme_LO_sober.R
@@ -16,9 +16,24 @@ theme_LO_sober <- ggplot2::theme(plot.title = element_text(size = 12, face="bold
                                  axis.text.x = element_text(size = 8),
                                  axis.text.y = element_text(size = 8),
                                  panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"),
-                                 panel.grid.major.y = element_line(size = 0.025, linetype = 'solid', colour = "grey"),
+                                 panel.grid.major.y = element_blank(),
                                  panel.grid.minor.y = element_blank(),
                                  panel.grid.major.x = element_blank(),
                                  panel.grid.minor.x = element_blank(),
                                  axis.line = element_line(colour = "grey30", size = 0.4, linetype = "solid"))
+
+theme_LO_adaptation <- ggplot2::theme(axis.title.x = element_text(size = 10),
+                                 axis.title.y = element_text(size = 10),
+                                 axis.text.x = element_text(size = 6),
+                                 axis.text.y = element_text(size = 8),
+                                 panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"),
+                                 panel.grid.major.x = element_blank(),
+                                 panel.grid.minor.x = element_blank(),
+                                 panel.grid.major.y = element_line(size = 0.1, linetype = 'solid',colour = "grey"),
+                                 panel.grid.minor.y = element_line(size = 0.05, linetype = 'solid',colour = "grey"),
+                                 #strip.background = element_rect(fill="white"),
+                                 axis.line = element_line(colour = "grey30", size = 0.4, linetype = "solid"),
+                                 legend.position='none',
+                                 strip.text.x = element_text(size=4, color="transparent"),
+                                 plot.title = element_text(size=10,face="bold",hjust = 0.5))
 
