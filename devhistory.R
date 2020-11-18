f88898fc1fac733d06b3968fc142880f87f48a9c
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-17 19:05:26 -0700

    :monkey: add new theme for LO plot

--- devhistory.R
+++ devhistory.R
@@ -21,6 +21,7 @@ usethis::use_r("sim_fitnessdata")
 usethis::use_r("boot_fitnessdata")
 usethis::use_r("computemeanvar_fitnessdata")
 usethis::use_r("computemeanvar_fitnessdifferencedata")
+usethis::use_r("theme_LO_sober")
 
 dir.create("data")
 dir.create("reports")
@@ -34,7 +35,7 @@ usethis::use_package("ggplot2")
 usethis::use_package("lme4")
 usethis::use_package("Rmisc")
 usethis::use_package("cowplot")
-usethis::use_package("MuMin")
+usethis::use_package("MuMIn")
 
 ## Update NAMESPACE file
 devtools::document()
