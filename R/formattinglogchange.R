260d905e1783de27ef3f2d7522484b1e599fa6d8
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-17 20:50:09 -0700

    :monkey: now this function creates also pairwise dataset

--- R/formattinglogchange.R
+++ R/formattinglogchange.R
@@ -1,17 +1,21 @@
-' @title Formatting a logchange dataset to test correlation
+' @title Formatting a logchange dataset to pairwise dataset for testing MA-regression
 #'
-#' @description  Formatting a logchange dataset to test correlation
-#' From a logchange dataset created using computelogchange() to dataset for testing MA-regression
+#' @description  Formatting a logchange dataset
+#' From a logchange dataset created using computelogchange() to pairwise dataset for testing MA-regression
 #'
 #' @param logchange_data Intermediate phenotyping dataset created using loadfitnessdata()
+#' @param generation Phenotyping step of interest
+#' @param fruit1 First fruit of the pairwise
+#' @param fruit2 Second fruit of the pairwise
 
-#' @return Dataset
+#' @return Dataset with logchange for two fruits during a specific phenotyping step
 #' @export
 #'
 #' @examples
-#'data_sum <- formattinglogchange(logchange_dataset = data_logchange)
+#'data_sum <- formattinglogchange(logchange_dataset = data_logchange, generation = "7", fruit1 = "Cherry",fruit2 = "Cranberry")
 
-formattinglogchange <- function(logchange_dataset = data_logchange){
+formattinglogchange <- function(logchange_dataset = data_logchange,
+                                generation = "7", fruit1 = "Cherry",fruit2 = "Cranberry"){
 
   #Temporary dataset
   TEMP_data_sum_G7G29_logchange<-logchange_dataset
@@ -27,6 +31,17 @@ formattinglogchange <- function(logchange_dataset = data_logchange){
   data_sum_G7G29_logchange_temp<-merge(TEMP_data_sum_G7G29_logchange,temp_symp,by="Line")
   data_sum_G7G29_logchange<-data_sum_G7G29_logchange_temp[data_sum_G7G29_logchange_temp$SA=="0",]
 
-  return(data_sum_G7G29_logchange)
+
+  #Create pairwise dataset
+  data_pairwise_generation<-data_sum_G7G29_logchange[data_sum_G7G29_logchange$Generation==generation&
+                                                       data_sum_G7G29_logchange$Symp==fruit1|
+                                                       data_sum_G7G29_logchange$Generation==generation&
+                                                       data_sum_G7G29_logchange$Symp==fruit2,]
+  data_pairwise_generation<-data_pairwise_generation[data_pairwise_generation$Allop==fruit1|
+                                                       data_pairwise_generation$Allop==fruit2,]
+
+
+  return(data_pairwise_generation)
+
 
 }
