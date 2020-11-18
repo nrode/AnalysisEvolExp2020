c83c0a2fde586174e3564f0799a399030ae71e13
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-18 08:43:23 -0700

    :fairy: add space

--- R/theme_LO_sober.R
+++ R/theme_LO_sober.R
@@ -9,15 +9,15 @@
 #'PLOT_MAIN_Text<-ggplot + theme_LO_sober
 
 #Theme
-theme_LO_sober <- ggplot2::theme(plot.title = element_text( size=12,face="bold",hjust = 0.5),
-                                 axis.title.x = element_text( size=10),
-                                 axis.title.y = element_text( size=10),
-                                 axis.text.x  = element_text( size=8),
-                                 axis.text.y  =  element_text( size=8),
-                                 panel.background = ggplot2::element_rect(fill = "white",colour = "white",size = 0.5, linetype = "solid"),
-                                 panel.grid.major.y = ggplot2::element_line(size = 0.025, linetype = 'solid',colour = "grey"),
+theme_LO_sober <- ggplot2::theme(plot.title = element_text(size = 12, face="bold", hjust = 0.5),
+                                 axis.title.x = element_text(size = 10),
+                                 axis.title.y = element_text(size = 10),
+                                 axis.text.x = element_text(size = 8),
+                                 axis.text.y = element_text(size = 8),
+                                 panel.background = ggplot2::element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"),
+                                 panel.grid.major.y = ggplot2::element_line(size = 0.025, linetype = 'solid', colour = "grey"),
                                  panel.grid.minor.y = element_blank(),
-                                 panel.grid.major.x= element_blank(),
-                                 panel.grid.minor.x= element_blank(),
+                                 panel.grid.major.x = element_blank(),
+                                 panel.grid.minor.x = element_blank(),
                                  axis.line = ggplot2::element_line(colour = "grey30", size = 0.4, linetype = "solid"))
 
