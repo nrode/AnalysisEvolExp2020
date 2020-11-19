0654a16860a8d06abb75762272e6d01d3accd08f
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-19 16:38:27 -0700

    :bone: new version with weighted mean and weighted sd

--- R/computelogchange_forlongdata.R
+++ R/computelogchange_forlongdata.R
@@ -1,6 +1,15 @@
 #' @title Calculation difference in fitness: from longitudinal data to logchange
 #'
-#' @description  Log change in fitness in generation x for the population i in the test fruit j = mean(W)Gxij / mean(W)G1j
+#' @description  Log change in fitness in the three phase for the population i in selective fruit j
+#'
+#' = log(mean(nb_adults_ij_allgenerationphase1/mean(nb_adlts_j_G2))
+#' = log(mean(nb_adults_ij_allgenerationphase3/mean(nb_adlts_j_G2))
+
+#' Example for CE2 during phase1
+#' log(mean (CE2 in G2 G3 G4 G5) / mean (all lines Cherry in G2))
+#'
+#' Mean and sd are weighted by the number of tubes counted
+#'
 #'
 #'
 #'
@@ -15,17 +24,28 @@
 computelogchange_forlongdata <- function(longitudinal_dataset = data_sum){
 
   # Remove pool step
-  longitudinal_dataset<-longitudinal_dataset[longitudinal_dataset$Step!="pool",]
+  #longitudinal_dataset<-longitudinal_dataset[longitudinal_dataset$Step!="pool",]
 
-  # Calculate mean during each step for each line
+  # Calculate weighted mean during each step for each line
   TEMP_data_step <- Rmisc::summarySE(longitudinal_dataset,
                                       measurevar = "Nb_adults",
                                       groupvars = c("Line", "Fruit_s", "Step"))
 
+  TEMP_data_step <- data.frame(Line=tapply(longitudinal_dataset$Line,longitudinal_dataset$Line,unique),
+                               Fruit_s=tapply(longitudinal_dataset$Fruit_s,longitudinal_dataset$Line,unique),
+                               Step=tapply(longitudinal_dataset$Step,longitudinal_dataset$Line,unique),
+                               N=tapply(longitudinal_dataset$N,longitudinal_dataset$Line,sum),
+                               Nb_adults=sapply(split(longitudinal_dataset,longitudinal_dataset$Line),
+                                           function(x) weighted.mean(x$Nb_adults,x$N)),
+                               sd=sapply(split(longitudinal_dataset,longitudinal_dataset$Line),
+                                         function(x) radiant.data::weighted.sd(x$Nb_adults,x$N)))
+
   # Calculate G2 mean and sd
   TEMP_G2 <- longitudinal_dataset[longitudinal_dataset$Generation=="2",]
-  TEMP_mean_G2 <- data.frame(Mean_G2 = tapply(TEMP_G2$Nb_adults, TEMP_G2$Fruit_s, mean),
-                             Sd_G2 = tapply(TEMP_G2$Nb_adults, TEMP_G2$Fruit_s, sd),
+  TEMP_mean_G2 <- data.frame(Mean_G2 = sapply(split(TEMP_G2,TEMP_G2$Fruit_s),
+                                              function(x) weighted.mean(x$Nb_adults,x$N)),
+                             Sd_G2 = sapply(split(TEMP_G2,TEMP_G2$Fruit_s),
+                                            function(x) radiant.data::weighted.sd(x$Nb_adults,x$N)),
                              N_G2 = tapply(TEMP_G2$N, TEMP_G2$Fruit_s, sum),
                              Fruit_s = rownames(tapply(TEMP_G2$Nb_adults, TEMP_G2$Fruit_s, mean)))
 
