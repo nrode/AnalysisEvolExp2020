a7a164f06d3fec62fc61a4844c17ddd99f9cd2c3
Laure Olazcuaga <laure@Laures-MacBook-Pro.local>
2020-11-18 19:34:27 -0700

    :fairy: increase police size axis plot

--- R/theme_LO_sober.R
+++ R/theme_LO_sober.R
@@ -10,11 +10,11 @@
 #'PLOT_MAIN_Text<-ggplot + theme_LO_sober
 
 #Theme
-theme_LO_sober <- ggplot2::theme(plot.title = element_text(size = 12, face="bold", hjust = 0.5),
-                                 axis.title.x = element_text(size = 10),
-                                 axis.title.y = element_text(size = 10),
-                                 axis.text.x = element_text(size = 8),
-                                 axis.text.y = element_text(size = 8),
+theme_LO_sober <- ggplot2::theme(plot.title = element_text(size = 13, face="bold", hjust = 0.5),
+                                 axis.title.x = element_text(size = 13),
+                                 axis.title.y = element_text(size = 13),
+                                 axis.text.x = element_text(size = 9),
+                                 axis.text.y = element_text(size = 9),
                                  panel.background = element_rect(fill = "white", colour = "white", size = 0.5, linetype = "solid"),
                                  panel.grid.major.y = element_blank(),
                                  panel.grid.minor.y = element_blank(),
@@ -23,7 +23,7 @@ theme_LO_sober <- ggplot2::theme(plot.title = element_text(size = 12, face="bold
                                  axis.line = element_line(colour = "grey30", size = 0.4, linetype = "solid"),
                                  #legend.justification =  c("right", "top"),
                                  legend.title = element_text(colour="black", size = 10, face = "bold"),
-                                 legend.text = element_text(colour="black", size = 8))
+                                 legend.text = element_text(colour="black", size = 10))
 
 
 theme_LO_adaptation <- ggplot2::theme(axis.title.x = element_text(size = 10),
