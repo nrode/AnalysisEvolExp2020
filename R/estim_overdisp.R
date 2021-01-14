ac5f5e42a063a04fbba7355de65617f3bc784bcc
Nicolas O. Rode <nicolas.rode@inrae.fr>
2021-01-14 09:28:38 +0100

    :dog: update function to also analyze the number of eggs

--- R/estim_overdisp.R
+++ R/estim_overdisp.R
@@ -5,6 +5,7 @@
 #' @param colfactorlevel Level of factor considered (e.g. cherry)
 #' @param data Dataset considered
 #' @param generation Generation considered
+#' @param trait Trait considered (default Nb_adults)
 #'
 #' @return "number_tubes_counted",
 #' "fitted_mean_nb_adults",
@@ -20,20 +21,32 @@
 #' @examples
 #' estim_overdisp(data=data_G0, colfactor="Treatment", colfactorlevel="Cherry", generation="G0")
 #'
-estim_overdisp <- function(colfactorlevel, colfactor, data, generation){
+estim_overdisp <- function(colfactorlevel, colfactor, data, generation, trait = "Nb_adults"){
   ## Subset data with the right factor level
   data <- data[data[, colfactor]==colfactorlevel,]
   ## Fit model
-  m <- MASS::glm.nb(Nb_adults~1, data=data)
+  if(trait=="Nb_adults"){
+    m <- MASS::glm.nb(Nb_adults~1, data=data)
+  }else{
+    if(trait=="Nb_eggs"){
+      m <- MASS::glm.nb(Nb_eggs~1, data=data)
+    }else{
+      m <- glm(cbind(Nb_adults, Nb_eggs)~1 , data=data, family="binomial")
+    }
+  }
+
   ## Get parameters
-  param <- c(length(data$Nb_adults),
-             as.numeric(exp(coef(m))),
-             m$theta,
-             var(data$Nb_adults),
-             as.numeric(exp(coef(m))+(exp(coef(m))^2)/m$theta),
-             var(data$Nb_adults)/mean(data$Nb_adults),
-             1+as.numeric(exp(coef(m)))/m$theta,
-             sqrt(length(data$Nb_adults))*(mean(data$Nb_adults)/sd(data$Nb_adults)),
-             ((4*length(data$Nb_adults)^(3/2))/(1+4*length(data$Nb_adults)))* (mean(data$Nb_adults)/sd(data$Nb_adults)))
-  as.vector(c(colfactorlevel, generation, round(param, 2)))
+    param <- c(nrow(data),
+               as.numeric(exp(coef(m))),
+               m$theta,
+               var(data[, trait]),
+               as.numeric(exp(coef(m))+(exp(coef(m))^2)/m$theta),
+               var(data[, trait])/mean(data[, trait]),
+               1+as.numeric(exp(coef(m)))/m$theta,
+               sqrt(nrow(data))*(mean(data[, trait])/sd(data[, trait])),
+               ((4*nrow(data)^(3/2))/(1+4*nrow(data)))* (mean(data[, trait])/sd(data[, trait])))
+
+    return(as.vector(c(colfactorlevel, generation, round(param, 2))))
+
+  c("factor", "generation", "number_tubes_counted", "fitted_mean_Emergence_rate", "observed_var_Emergence_rate", "fitted_var_Emergence_rate", "obsoverdisp", "fittedoverdisp", "standardized_mean", "standardized_mean_small_sample")
 }
