3ea7a137110683ba3ec615c83d4aaf6d585d153b
Nicolas O. Rode <nicolas.rode@inrae.fr>
2021-01-15 10:55:20 +0100

    :dog: add estimates to the data.frame returned by the functions

--- R/bCI_tab.R
+++ R/bCI_tab.R
@@ -16,12 +16,12 @@
 bCI_tab <- function(b,ind=length(b$t0), type="perc", conf=0.95) {
 btab0 <- t(sapply(as.list(seq(ind)),
                   function(i)
-                    boot::boot.ci(b,index=i,conf=conf, type=type)$percent))
+                    boot::boot.ci(b, index=i, conf=conf, type=type)$percent))
 btab <- btab0[,4:5]
 rownames(btab) <- names(b$t0)
 a <- (1 - conf)/2
 a <- c(a, 1 - a)
 pct <- stats:::format.perc(a, 3)
 colnames(btab) <- pct
-return(btab)
+return(data.frame(Estimate=b$t0, btab))
 }
